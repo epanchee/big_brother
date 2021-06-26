0a52b1b265e2af0c59a33fcb1e595b612be9e182
Timofey Epanchintsev <epanchee@gmail.com>
2021-06-26 11:36:57 +0500

    Added(config_parser): multiple configs in directory

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use crate::slaves::*;
 
 fn main() {
     // fetchers::main();
-    // config_parser::serialize();
-    println!("{:#?}", config_parser::parse_yaml("configs/example.yaml"));
+    // println!("{:#?}", config_parser::parse_yaml("configs/example.yaml"));
+    println!("{:#?}", config_parser::parse_config_dir("configs"));
 }
--- src/slaves/config_parser.rs
+++ src/slaves/config_parser.rs
@@ -1,15 +1,44 @@
-use std::fs;
+use std::{fs, path::Path};
+
+use anyhow::{anyhow, Result};
 
 use crate::slaves::fetchers::FetchItem;
 
-pub fn parse_yaml(config_file: &str) -> Vec<FetchItem> {
+type Config = Vec<FetchItem>;
+
+pub fn parse_yaml(config_file: &str) -> Config {
     let content = fs::read_to_string(config_file).unwrap();
     serde_yaml::from_str(&content[..]).unwrap()
 }
 
+pub fn parse_config_dir(dir_str: &str) -> Result<Vec<Config>> {
+    let dir = Path::new(dir_str);
+    let mut configs: Vec<Config> = vec![];
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let path = entry?.path();
+            let ext = path
+                .extension()
+                .ok_or(anyhow!("Path to str conversion error"))?;
+            if ext == "yaml" {
+                configs.push(parse_yaml(
+                    path.to_str()
+                        .ok_or(anyhow!("Path to str conversion error"))?,
+                ));
+            }
+        }
+    } else {
+        panic!("{} is not a directory!", dir_str)
+    }
+    Ok(configs)
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::slaves::{config_parser::parse_yaml, fetchers::FetchItem};
+    use crate::slaves::{
+        config_parser::{parse_config_dir, parse_yaml},
+        fetchers::FetchItem,
+    };
 
     #[test]
     fn test_parse_yaml() {
@@ -35,5 +64,55 @@ mod tests {
         let mut fetch_items = parse_yaml("configs/example.yaml");
         fetch_items.sort();
         assert_eq!(vec![item1, item2, item3], fetch_items);
+    }
+
+    #[test]
+    fn test_parse_config_dir() {
+        let item1 = FetchItem {
+            name: "item1".to_string(),
+            path: "body > div > p:nth-child(3) > a".to_string(),
+            primary: false,
+            item_type: "".to_string(),
+            related: vec![],
+        };
+        let item2 = FetchItem {
+            name: "item2".to_string(),
+            ..item1.clone()
+        };
+        let item3 = FetchItem {
+            name: "item3".to_string(),
+            related: vec![Box::new(item1.clone()), Box::new(item2.clone())],
+            ..item1.clone()
+        };
+        let item_x = FetchItem {
+            name: "entity_x".to_string(),
+            path: "body > div > p:nth-child(3) > a".to_string(),
+            primary: false,
+            item_type: "".to_string(),
+            related: vec![],
+        };
+        let item_y = FetchItem {
+            name: "entity_y".to_string(),
+            related: vec![Box::new(item_x.clone())],
+            ..item1.clone()
+        };
+        let item_z = FetchItem {
+            name: "entity_z".to_string(),
+            related: vec![Box::new(item_y.clone())],
+            primary: true,
+            ..item1.clone()
+        };
+
+        let config1 = vec![item1, item2, item3];
+        let config2 = vec![item_x, item_y, item_z];
+
+        let mut configs = parse_config_dir("configs").unwrap();
+        configs.sort();
+
+        assert_eq!(vec![config2, config1], configs);
+
+        // detailed test
+        // configs[1].iter().zip(&config1).for_each(|(i1, i2)| assert_eq!(i1, i2));
+        // configs[0].iter().zip(&config2).for_each(|(i1, i2)| assert_eq!(i1, i2));
     }
 }
