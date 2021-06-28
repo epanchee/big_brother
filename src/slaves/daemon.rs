a71d70e39f1f1fb996b8c50dc771af457e7b56a2
Timofey Epanchintsev <t.epanchintsev@carbonsoft.ru>
2021-06-28 17:23:42 +0500

    Added(daemon): first version

--- src/slaves/daemon.rs
+++ src/slaves/daemon.rs
@@ -1,6 +1,8 @@
-use std::time::Duration;
+use std::{sync::Arc, thread, time::Duration};
 
-use super::config_parser::parse_config_dir;
+use tokio::sync::Mutex;
+
+use super::{config_parser::parse_config_dir, fetchers::BaseFetcher};
 
 pub struct FetchDaemon {
     interval: Duration,
@@ -22,10 +24,28 @@ impl FetchDaemon {
         }
     }
 
+    async fn fetch_data(configs: Arc<Mutex<Vec<BaseFetcher>>>) {
+        for config in configs.try_lock().unwrap().iter() {
+            let fetched = config
+                .fetch()
+                .await
+                .map(|list| list.iter().cloned().flatten().collect::<Vec<_>>());
+            if let Ok(fetched) = fetched {
+                todo!()
+            } else {
+                println!("Couldn't fetch any data in {:?}", config)
+            }
+        }
+    }
+
     pub fn start(self) {
         let configs = parse_config_dir(&self.conf_path[..]);
-
+        let configs = Arc::new(Mutex::new(configs));
         loop {
+            let configs = configs.clone();
+            tokio::spawn(async move { Self::fetch_data(configs).await });
+            print!("Going to sleep for {} secs...", self.interval.as_secs());
+            thread::sleep(self.interval)
         }
     }
 }
