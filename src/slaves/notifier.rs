56cf4b52fc2149eb92f3139e682bce7f3bb24b34
Timofey Epanchintsev <epanchee@gmail.com>
2021-07-20 18:33:11 +0500

    Added(notifier): using Rutebot

--- src/slaves/notifier.rs
+++ src/slaves/notifier.rs
@@ -1,7 +1,8 @@
-use std::{fmt::Display, sync::Arc};
+use std::fmt::Display;
 
 use anyhow::Result;
 
+use rutebot::{client::Rutebot, requests::SendMessage, responses::Message};
 use tokio::sync::mpsc::{self, Receiver, Sender};
 
 #[derive(Clone, Copy)]
@@ -21,27 +22,35 @@ impl Display for Signal {
     }
 }
 
+struct RutebotWrapper(Rutebot, String);
+
 pub struct TgNotifier {
     tx: Sender<Signal>,
 }
 
 impl TgNotifier {
-    pub fn new(token: &str) -> Self {
+    pub fn new(token: &str, chat_id: &str) -> Self {
         let (tx, rx) = mpsc::channel(32);
-        Self::start(rx);
-        Self { tx }
+        let bot = RutebotWrapper(Rutebot::new(token), chat_id.to_string());
+        let notifier = Self { tx };
+        Self::start(rx, bot);
+        notifier
     }
 
-    fn start(mut rx: Receiver<Signal>) {
+    fn start(mut rx: Receiver<Signal>, bot: RutebotWrapper) {
         tokio::spawn(async move {
             while let Some(signal) = rx.recv().await {
-                Self::process_signal(signal).await.unwrap();
+                Self::process_signal(signal, &bot).await.unwrap();
             }
         });
     }
 
-    async fn process_signal(signal: Signal) -> Result<()> {
+    async fn process_signal(signal: Signal, bot: &RutebotWrapper) -> Result<()> {
         println!("{}", signal);
+/*         bot.0
+            .prepare_api_request(SendMessage::new(&bot.1[..], &signal.to_string()[..]))
+            .send()
+            .await?; */
         Ok(())
     }
 }
@@ -56,7 +65,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_notifier() {
-        let notifier = TgNotifier::new("");
+        let notifier = TgNotifier::new("", "");
         let tx2 = notifier.tx.clone();
         let tx3 = notifier.tx.clone();
 
