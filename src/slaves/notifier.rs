bfe78d8941dbb5d03af57e0a8c2bfc509c340a52
Timofey Epanchintsev <epanchee@gmail.com>
2021-07-22 12:33:20 +0500

    Added(notifier): working version

--- src/slaves/notifier.rs
+++ src/slaves/notifier.rs
@@ -2,8 +2,11 @@ use std::fmt::Display;
 
 use anyhow::{anyhow, Result};
 
-use rutebot::{client::Rutebot, requests::SendMessage, responses::Message};
-use tokio::sync::mpsc::{self, Sender};
+use rutebot::{client::Rutebot, requests::SendMessage};
+use tokio::{
+    sync::mpsc::{self, Receiver, Sender},
+    task::JoinHandle,
+};
 
 #[derive(Clone, Copy, Debug)]
 pub enum Signal<T: Display = String> {
@@ -26,35 +29,35 @@ impl Display for Signal {
 struct RutebotWrapper(Rutebot, String);
 
 #[derive(Clone)]
-pub struct TgNotifier<T: Display = String> {
-    tx: Option<Sender<Signal<T>>>,
-    bot: RutebotWrapper,
+pub struct TgNotifier<T: Display + Send = String> {
+    tx: Sender<Signal<T>>,
 }
 
 impl<T> TgNotifier<T>
 where
-    T: Display + Send,
+    T: Display + Send + 'static,
     Signal<T>: Display,
 {
     pub fn new(token: &str, chat_id: &str) -> Self {
-        let bot = RutebotWrapper(Rutebot::new(token), chat_id.to_string());
-        Self { tx: None, bot }
+        Self::new_with_loop_handle(token, chat_id).0
     }
 
-    fn start(&'static mut self) {
+    pub fn new_with_loop_handle(token: &str, chat_id: &str) -> (Self, JoinHandle<()>) {
+        let (tx, rx) = mpsc::channel(32);
+        let bot = RutebotWrapper(Rutebot::new(token), chat_id.to_string());
+        (Self { tx }, Self::create_channel_loop(rx, bot))
+    }
+
+    fn create_channel_loop(mut rx: Receiver<Signal<T>>, bot: RutebotWrapper) -> JoinHandle<()> {
         tokio::spawn(async move {
-            let (tx, mut rx) = mpsc::channel(32);
-            self.tx = Some(tx);
             while let Some(signal) = rx.recv().await {
-                Self::process_signal(signal, &self.bot).await.unwrap();
+                Self::process_signal(signal, &bot).await.unwrap();
             }
-        });
+        })
     }
 
     async fn send(&self, signal: Signal<T>) -> Result<()> {
         self.tx
-            .as_ref()
-            .ok_or_else(|| anyhow!("No tx avalible"))?
             .send(signal)
             .await
             .map_err(|_| anyhow!("Send error"))
@@ -62,10 +65,11 @@ where
 
     async fn process_signal(signal: Signal<T>, bot: &RutebotWrapper) -> Result<()> {
         println!("{}", signal);
-        /*         bot.0
-        .prepare_api_request(SendMessage::new(&bot.1[..], &signal.to_string()[..]))
-        .send()
-        .await?; */
+        let msg = &signal.to_string()[..];
+        bot.0
+            .prepare_api_request(SendMessage::new(&bot.1[..], msg))
+            .send()
+            .await?;
         Ok(())
     }
 }
@@ -80,7 +84,9 @@ mod tests {
 
     #[tokio::test]
     async fn test_notifier() {
-        let notifier = TgNotifier::new("", "");
+        let (notifier, loop_h) = TgNotifier::new_with_loop_handle(
+
+        );
         let notifier2 = notifier.clone();
 
         let futures = vec![
@@ -94,5 +100,7 @@ mod tests {
         ];
 
         try_join_all(futures).await.unwrap();
+        drop(notifier);
+        loop_h.await.unwrap();
     }
 }
